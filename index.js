f478a9c45e96d8291a2cf643f9b35b7411ebe72f
Joan Cortes <js.199911@gmail.com>
2021-10-29 12:12:03 -0500

    Creando el servidor

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+/* const Persona = require('./models/Persona.js');
+ */
+const app = express();
+
+const router = express.Router();
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+mongoose.connect("mongodb+srv://joan:cali199911@clusterprogweb.qka5w.mongodb.net/inscripcion?retryWrites=true&w=majority");
+
+app.listen(3003, () => {
+    console.log("Servidor funciona");
+});
